8c98cc2f0e1dd180fcd68c4f2cc9c12adbbd3cd1
ChngWC123 <chngwc123@gmail.com>
2023-04-08 00:45:52 +0800

    defendants can now give in to disputes

--- contracts/data_storages/RideHailingDisputesDataStorage.sol
+++ contracts/data_storages/RideHailingDisputesDataStorage.sol
@@ -9,6 +9,9 @@ contract RideHailingDisputesDataStorage is DataStorageBaseContract {
         address defendant;
         string complaintDescription;
         string defenseDescription;
+        bool responded;
+        bool carFeeDispute;
+        bool compensationDispute;
         bool resolved;
         uint256 plaintiffVotes;
         uint256 defendantVotes;
@@ -31,6 +34,9 @@ contract RideHailingDisputesDataStorage is DataStorageBaseContract {
                 complaintDescription,
                 "",
                 false,
+                carFeeDispute,
+                compensationDispute,
+                false,
                 0,
                 0,
                 new address[](0),
@@ -45,6 +51,56 @@ contract RideHailingDisputesDataStorage is DataStorageBaseContract {
         uint256 disputeId
     ) external view internalContractsOnly returns (Dispute memory) {
         return disputeData[disputeId];
+    }
+
+    function getRideId(
+        uint256 disputeId
+    ) external view internalContractsOnly returns (uint) {
+        return disputeData[disputeId].rideId;
+    }
+
+    function setDisputeResponse (
+        uint256 disputeId
+    ) external internalContractsOnly {
+        disputeData[disputeId].responded = true;
+    }
+
+    function getNumOfDefedantUnresponded( //check if have respond to disputes
+        address defendant
+    ) external view internalContractsOnly returns (uint256) {
+        uint unrespondCounter = 0;
+        for(uint i = 0; i < disputeData.length; i++) {
+            if(disputeData[i].defendant == defendant && disputeData[i].responded == false)
+            unrespondCounter ++;
+        }
+        return unrespondCounter;
+    }
+
+    function getTimeRemaining(uint256 disputeId) external view internalContractsOnly returns (uint256) {
+        uint256 timePassed = block.timestamp - disputeData[disputeId].startTime;
+        if(timePassed >= 1 days) {
+            return 0;
+        } else {
+            return 1 days - timePassed;
+        }
+    }
+
+
+    function generateDisputeForVoting() external view returns (uint256) {
+        // dummy oracle : get unresolved dispute by rng
+        for (uint256 i = 0; i < disputeData.length; i++) {
+            if (disputeData[i].resolved != true) {
+                return i; // i is the disputeId
+            }
+        }
+        // returns max uint256 value if no unresolved dispute found
+        return (2 ^ (256 - 1));
+    }
+
+    function checkDisputeStatus(
+        uint256 disputeId
+    ) external view returns (bool) {
+        return disputeData[disputeId].resolved;
     }
 
     function setDefenseDescription(
